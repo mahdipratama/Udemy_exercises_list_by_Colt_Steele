cbf1745d5ac48fd7bf36a1562454e9791a97df85
mahdipratama <mahdipratama29@gmail.com>
2022-08-28 11:17:26 +0700

    adding Javascript basic conditional and exercise

---
+++ 06-JavaScript_Basics/Exercises/script.js
@@ -0,0 +1,12 @@
+function isEven(num) {
+  //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+  if (num % 2 === 0) {
+    console.log('even');
+  } else {
+    console.log('odd');
+  }
+}
+
+isEven(4);
+
+console.log(8 % 2);
---
+++ 06-JavaScript_Basics/Running_Js_Script/app.js
@@ -0,0 +1,73 @@
+console.log("HELLO FROM OUR FIRST JS FILE!!!!");
+let total = 1 + 3;
+console.log("GOODBYE!");
+
+// =====================
+// BASIC IF/ELSE
+// =====================
+let random = Math.random();
+if (random < 0.5) {
+  console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
+} else {
+  console.log("YOUR NUMBER IS GREATER (OR EQUAL) THAN 0.5!!!")
+}
+console.log(random);
+
+// =====================
+// PROMPT EXAMPLE
+// =====================
+
+const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+
+if (dayOfWeek === 'monday') {
+  alert("UGHHH I HATE MONDAYS!")
+} else if (dayOfWeek === 'saturday') {
+  alert("YAY I LOVE SATURDAYS!")
+} else if (dayOfWeek === 'friday') {
+  alert("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!")
+} else {
+  alert("MEH")
+}
+
+
+
+// =====================
+// TICKET PRICE EXAMPLE
+// =====================
+
+// 0-5 - FREE
+// 5 - 10 CHILD $10 
+// 10 - 65 ADULT $20 
+// 65+ SENIOR $10
+const age = 890;
+
+if (age < 5) {
+  console.log("You are a baby.  You get in for free!")
+} else if (age < 10) {
+  console.log("You are a child.  You pay $10")
+} else if (age < 65) {
+  console.log("You are an adult. You pay $20")
+} else {
+  console.log("You are a senior. You pay $10")
+}
+
+// =====================
+// NESTING CONDITIONALS
+// =====================
+
+const password = prompt("please enter a new password");
+
+// Password must be 6+ characters
+if (password.length >= 6) {
+  // Password cannot include space
+  if (password.indexOf(' ') === -1) {
+    console.log("Valid Password!");
+  } else {
+    console.log("Password cannot contain spaces!")
+  }
+} else {
+  console.log("PASSWORD TOO SHORT! Must be 6+ characters")
+}
+
+
+
