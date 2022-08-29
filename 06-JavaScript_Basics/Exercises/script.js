7c4fb411152608e82550d7d7eb295c8d0f2a8a9e
mahdipratama <mahdipratama29@gmail.com>
2022-08-29 10:43:40 +0700

    finished exercise #2

--- 06-JavaScript_Basics/Exercises/script.js
+++ 06-JavaScript_Basics/Exercises/script.js
@@ -1,3 +1,4 @@
+// Exercise #1
 function isEven(num) {
   //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
   if (num % 2 === 0) {
@@ -10,3 +11,17 @@ function isEven(num) {
 isEven(4);
 
 console.log(8 % 2);
+
+
+// Exercise #2 
+function getColor(phrase) {
+  if (phrase == 'stop') {
+    console.log('red');
+  } else if (phrase == 'slow') {
+    console.log('yellow');
+  } else if (phrase == 'go') {
+    console.log('green');
+  } else console.log('purple');
+}
+
+getColor('slow'); // yellow
