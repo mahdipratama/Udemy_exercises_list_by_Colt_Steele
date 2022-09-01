98508b3c4d21e5f0ec597d6a6a564f19dcf189b3
mahdipratama <mahdipratama29@gmail.com>
2022-09-01 11:55:52 +0700

    create Loops index and script

---
+++ 06-JavaScript_Basics/Loops/ForLoops/app.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+
+// Printing I Love You 3000
+let times = 3000;
+for (let i = 1; i <= times; i++) {
+  console.log('I Love You! ' + i);
+}   
