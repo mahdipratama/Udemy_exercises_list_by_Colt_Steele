a122b04e899f294b6c34dbc750a4be3cec62bc9c
mahdipratama <mahdipratama29@gmail.com>
2022-09-01 16:27:46 +0700

    Update app.js

--- 06-JavaScript_Basics/Loops/ForLoops/app.js
+++ 06-JavaScript_Basics/Loops/ForLoops/app.js
@@ -1,10 +1,18 @@
-for (let i = 1; i <= 10; i++) {
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+
+// // Printing I Love You 3000
+// let times = 3000;
+// for (let i = 1; i <= times; i++) {
+//   console.log('I Love You! ' + i);
+// }   
+
+// for (let i = 2; i <= 20; i+= 2) {
+//   console.log(i);
+// }
+
+for (let i = 100; i >= 0; i -= 10) {
   console.log(i);
-}
-
-
-// Printing I Love You 3000
-let times = 3000;
-for (let i = 1; i <= times; i++) {
-  console.log('I Love You! ' + i);
-}   
+} 
