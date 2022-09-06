ed37f617054054bd156a7cf1d37937974ed053df
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-06 10:12:35 +0700

    update loops/for..of loop

--- 06-JavaScript_Basics/Loops/app.js
+++ 06-JavaScript_Basics/Loops/app.js
@@ -209,5 +209,39 @@
 
 
 // ===================================
-// WHILE LOOP
+// FOR ...OF LOOP
 // ===================================
+
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
+
+for (let i = 0; i < subreddits.length; i++) {
+  console.log(`Visit reddit.com/r/${subreddits[i]}`)
+};
+
+
+//Using For...of Loop
+for (let sub of subreddits) {
+  console.log(`Visit reddit.com/r/${sub}`);
+}
+
+
+
+const seatingChart = [
+  ['Kristen', 'Erik', 'Namita'],
+  ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+  ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let i = 0; i < seatingChart.length; i++) {
+  const row = seatingChart[i];
+  for (let j = 0; j < row.length; j++) {
+    console.log(row[j]);
+  }
+}
+
+// Using For...Of Loop 
+for (let row of seatingChart) {
+  for (let student of row) {
+    console.log(student);
+  }
+}
