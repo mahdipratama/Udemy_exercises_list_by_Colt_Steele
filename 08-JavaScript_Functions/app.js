52a32fc55750127d11ca64fc2dbe91b6447cf757
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-08 11:25:44 +0700

    update app.js note

--- 08-JavaScript_Functions/app.js
+++ 08-JavaScript_Functions/app.js
@@ -1,4 +1,13 @@
 function addNums(num1, num2) {
   return num1 + num2
 }
-addNums(5, 3);
+addNums(5, 3); // 8
+
+
+function greet(firstName, lastName) {
+  console.log(`Hey there, ${firstName} ${lastName[0]}.`);
+}
+greet('Nami', 'Saputri'); // Nami S.
+
+
+
