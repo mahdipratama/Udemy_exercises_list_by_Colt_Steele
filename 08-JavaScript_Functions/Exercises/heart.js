c6911a9226f96e702a1a3a789ac18d41ed502cd3
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-08 09:58:15 +0700

    finish Heart Function Exercise

---
+++ 08-JavaScript_Functions/Exercises/heart.js
@@ -0,0 +1,12 @@
+
+/*
+Heart Function Exercise
+--Define a function called printHeart that prints out the string "<3"
+--Execute your function once
+*/
+// Write your function here:
+function printHeart() {
+  console.log('<3')
+}
+
+printHeart();
