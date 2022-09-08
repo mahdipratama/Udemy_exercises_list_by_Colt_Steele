74d33bc01b6e0c649c6f2c2f6e16e1aaf3a9de7a
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-08 12:03:24 +0700

    finish Last Element Exercise

--- 08-JavaScript_Functions/Exercises/heart.js
+++ 08-JavaScript_Functions/Exercises/heart.js
@@ -99,3 +99,24 @@ function isShortsWeather(temperature) {
 isShortsWeather(80)
 isShortsWeather(48)
 isShortsWeather(75)
+
+
+/*
+Last Element Exercise
+=======================
+Please write a function called lastElement which accepts a single array argument.  The function should return the last element of the array (without removing the element).  If the array is empty, the function should return null.
+
+-lastElement([3,5,7]) //7
+-lastElement([1]) //1
+-lastElement([]) //null
+*/
+
+function lastElement(array) {
+  if (array.length === 0) {
+    return null
+  } else
+    return array[array.length - 1]
+}
+lastElement([3, 5, 7]) //7
+lastElement([1]) //1
+lastElement([]) //null
