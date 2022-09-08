b9310cc4135e25096eeb39979b13dd8675961b55
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-08 10:29:58 +0700

    finish rant function exercise

--- 08-JavaScript_Functions/Exercises/heart.js
+++ 08-JavaScript_Functions/Exercises/heart.js
@@ -1,6 +1,7 @@
 
 /*
 Heart Function Exercise
+=======================
 --Define a function called printHeart that prints out the string "<3"
 --Execute your function once
 */
@@ -10,3 +11,20 @@ function printHeart() {
 }
 
 printHeart();
+
+/*
+Rant Function Exercise
+=======================
+--Define a function called rant which accepts a string argument called message.  The function should print out an uppercased version of message 3 times (with 3 separate calls to console.log).  For example, rant("I hate beets") should print out:
+
+I HATE BEETS
+I HATE BEETS
+I HATE BEETS
+*/
+
+function rant(message) {
+  console.log(message.toUpperCase())
+  console.log(message.toUpperCase())
+  console.log(message.toUpperCase())
+}
+rant('I HATE BEETS');
