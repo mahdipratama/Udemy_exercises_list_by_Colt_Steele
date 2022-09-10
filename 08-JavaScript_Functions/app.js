305b17b87a0158b58b67810111ea44a3003a41c0
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-10 10:46:27 +0700

    update app.js note Higher Order Functions

--- 08-JavaScript_Functions/app.js
+++ 08-JavaScript_Functions/app.js
@@ -10,4 +10,21 @@ function greet(firstName, lastName) {
 greet('Nami', 'Saputri'); // Nami S.
 
 
+// Higher Order Function 
+function callTwice(func) {
+  func();
+  func();
+}
 
+function callTenTimes(f) {
+  for (let i = 0; i < 10; i++) {
+    f()
+  }
+}
+
+function rollDie() {
+  const roll = Math.floor(Math.random() * 6) + 1
+  console.log(roll);
+}
+
+callTenTimes(rollDie);
