61ff0f180327c2c537dd4a671b8b2ece4b74eee8
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-12 11:06:20 +0700

    update app.js note Using Try/Catch

--- 08-JavaScript_Functions/app.js
+++ 08-JavaScript_Functions/app.js
@@ -28,3 +28,13 @@ function rollDie() {
 }
 
 callTenTimes(rollDie);
+
+
+// Try/Catch 
+function yell(msg) {
+  try {
+    console.log(msg.toUpperCase().repeat(3));
+  } catch (e) {
+    console.log('Please pass a string next time');
+  }
+}
