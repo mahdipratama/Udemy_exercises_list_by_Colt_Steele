01925c3e70b312982aae21f07247ccc3a737a4aa
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-13 14:47:43 +0700

    update app note.js The ForEach Method

--- 08-JavaScript_Functions/app.js
+++ 08-JavaScript_Functions/app.js
@@ -38,3 +38,47 @@ function yell(msg) {
     console.log('Please pass a string next time');
   }
 }
+
+
+// The ForEach Method 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+// const print = element => console.log(element)
+
+// numbers.forEach(print)
+
+numbers.forEach(function (el) {
+  if (el % 2 === 0) {
+    console.log(el)
+  }
+})
+
+
+const movies = [
+  {
+    title: 'Amadeus',
+    score: 99
+  },
+  {
+    title: 'Stand By Me',
+    score: 85
+  },
+  {
+    title: 'Parasite',
+    score: 95
+  },
+  {
+    title: 'Alien',
+    score: 90
+  }
+]
+
+// const isMovie = function (movie) {
+//   console.log(`${movie.title} - ${movie.score}/100`);
+// }
+// movies.forEach(isMovie);
+
+
+movies.forEach(function (movie) {
+  console.log(`${movie.title} - ${movie.score}/100`)
+})
