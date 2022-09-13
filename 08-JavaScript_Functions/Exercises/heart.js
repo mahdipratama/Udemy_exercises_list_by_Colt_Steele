25f08f0ceeae8ed2f6048ed4ef39ee4744b6684f
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-13 15:28:23 +0700

    finish Map Practice Exercise

--- 08-JavaScript_Functions/Exercises/heart.js
+++ 08-JavaScript_Functions/Exercises/heart.js
@@ -271,7 +271,7 @@ layAnEgg should be a method which increments the value of eggCount by 1 and retu
 const hen = {
   name: 'Helen',
   eggCount: 0,
-  layAnEgg () {
+  layAnEgg() {
     this.eggCount++
     return 'EGG'
   }
@@ -282,3 +282,63 @@ console.log(hen.layAnEgg());
 console.log(hen.layAnEgg());
 console.log(hen.layAnEgg());
 console.log(hen.eggCount);
+
+
+/*
+Map Practice
+=======================
+Define a variable named firstNames and assign it to the result of mapping over the existing array, fullNames, so that firstNames contains only the first names of the Harry Potter characters from the fullNames array.
+
+e.g.,
+
+console.log(firstNames); // ['Albus', 'Harry', 'Hermione', 'Ron', 'Rubeus', 'Minerva', 'Severus']
+
+
+Please note:
+
+The fullNames array is an array of objects with each object containing properties for the first and last names of each character. You may need to click the "Reset code" link if you do not see the fullNames array pre-loaded into the exercise's index.js file.
+
+This exercise has been updated, you may see Q&A threads from this lecture that relate to the original exercise which no longer exists. They can be ignored.
+*/
+
+// DO NOT ALTER THE FOLLOWING CODE:
+const fullNames = [
+  {
+    first: 'Albus',
+    last: 'Dumbledore'
+  },
+  {
+    first: 'Harry',
+    last: 'Potter'
+  },
+  {
+    first: 'Hermione',
+    last: 'Granger'
+  },
+  {
+    first: 'Ron',
+    last: 'Weasley'
+  },
+  {
+    first: 'Rubeus',
+    last: 'Hagrid'
+  },
+  {
+    first: 'Minerva',
+    last: 'McGonagall'
+  },
+  {
+    first: 'Severus',
+    last: 'Snape'
+  }];
+
+// Write your code here
+
+// const firstNames = fullNames.map(function (orang) {
+//   return orang.first;
+// });
+
+const firstNames = fullNames.map(orang => orang.first);
+
+console.log(firstNames);
+console.log(typeof firstNames)
