6ecb65f2c4d86f5c7d0001e4c1b74d220e94033e
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-16 17:19:18 +0700

    update app.js note Reduce Methods

--- 08-JavaScript_Functions/app.js
+++ 08-JavaScript_Functions/app.js
@@ -217,3 +217,31 @@ movies.some(movie => movie.year > 2015)
 const exams = [80, 98, 92, 78, 77, 90, 89, 84, 81, 77]
 
 exams.every(score => score >= 75)
+
+
+// Reduce Methods
+const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
+
+// let total = 0;
+// for (let price of prices) {
+//     total += price
+// }
+// console.log(total)
+
+// const total = prices.reduce((total, price) => {
+//     return total + price
+// })
+
+const total = prices.reduce((total, price) => total + price)
+
+const minPrice = prices.reduce((min, price) => {
+  if (price < min) {
+    return price;
+  }
+  return min;
+})
+
+// We can provide an initial value as the 2nd arg to reduce:
+const evens = [2, 4, 6, 8];
+evens.reduce((sum, num) => sum + num) //20
+evens.reduce((sum, num) => sum + num, 100) //120
