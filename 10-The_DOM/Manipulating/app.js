9e3d84b5d82dd10dae0a106d4a5423d1deae728c
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-22 15:37:33 +0700

    udpate app.js note querySelectol & querySelectorAll

--- 10-The_DOM/Manipulating/app.js
+++ 10-The_DOM/Manipulating/app.js
@@ -5,9 +5,45 @@
 //   img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
 // }
 
-// Selecting all 'square' id
-const squareImages = document.getElementsByClassName('square');
+// // Selecting all 'square' class
+// const squareImages = document.getElementsByClassName('square');
 
-for (let img of squareImages) {
-  img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
+// for (let img of squareImages) {
+//   img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
+// }
+
+
+// Selecting any type single element
+
+const headingImg = document.querySelector('img')
+console.log(headingImg);
+
+const secondImg = document.querySelector('img:nth-of-type(2)')
+console.log(secondImg);
+
+const titleJava = document.querySelector('a[title="Java"]')
+console.log(titleJava);
+
+const firstBanner = document.querySelector('#banner');
+console.log(firstBanner);
+
+const firstSquare = document.querySelector('.square');
+console.log(firstBanner);
+
+
+// Selecting any type all elements
+
+const allP = document.querySelectorAll('p');
+console.log(allP);
+
+const allImg = document.querySelectorAll('img');
+console.log(allImg);
+
+// -selecting all 'a' nested inside of 'p'
+
+const links = document.querySelectorAll('p a')
+console.log(links);
+
+for (let link of links) {
+  console.log(link.href);
 }
