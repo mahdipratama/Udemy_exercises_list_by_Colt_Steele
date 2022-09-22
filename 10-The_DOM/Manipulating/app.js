d08bdbf99304d914174a0411bceb3cc302806e2d
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-22 14:53:10 +0700

    create The_DOM and update app.js note getElementById, getElementsByTagName & className

---
+++ 10-The_DOM/Manipulating/app.js
@@ -0,0 +1,13 @@
+// Selecting all 'img' tag name
+// const allImage = document.getElementsByTagName('img'); 
+
+// for (let img of allImage) {
+//   img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
+// }
+
+// Selecting all 'square' id
+const squareImages = document.getElementsByClassName('square');
+
+for (let img of squareImages) {
+  img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg'
+}
