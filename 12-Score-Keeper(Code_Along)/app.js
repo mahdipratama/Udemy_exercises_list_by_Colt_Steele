39e79790040aed2f702a89132504704551cf7631
Mahdi Pratama <mahdipratama29@gmail.com>
2022-10-10 17:30:49 +0700

    Refactoring DRY principle

--- 12-Score-Keeper(Code_Along)/app.js
+++ 12-Score-Keeper(Code_Along)/app.js
@@ -1,44 +1,44 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
+const p1 = {
+  score: 0,
+  button: document.querySelector('#p1Button'),
+  display: document.querySelector('#p1Display')
+}
+const p2 = {
+  score: 0,
+  button: document.querySelector('#p2Button'),
+  display: document.querySelector('#p2Display')
+}
+
 const resetButton = document.querySelector('#reset');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
 const winningScoreSelect = document.querySelector('#playto');
 
 
-let p1Score = 0;
-let p2Score = 0;
 let winningSCore = 3;
 let isGameOver = false;
 
 
-p1Button.addEventListener('click', function () {
+function updateScores(player, opponent) {
   if (!isGameOver) {
-    p1Score += 1;
-    if (p1Score === winningSCore) {
+    player.score += 1;
+    if (player.score === winningSCore) {
       isGameOver = true;
-      p1Display.classList.add('has-text-success');
-      p2Display.classList.add('has-text-danger');
-      p1Button.disabled = true;
-      p2Button.disabled = true;
+      player.display.classList.add('has-text-success');
+      opponent.display.classList.add('has-text-danger');
+      player.button.disabled = true;
+      opponent.button.disabled = true;
     }
-    p1Display.textContent = p1Score;
+    player.display.textContent = player.score;
   }
+};
+
+
+p1.button.addEventListener('click', function () {
+  updateScores(p1, p2);
 });
 
 
-p2Button.addEventListener('click', function () {
-  if (!isGameOver) {
-    p2Score += 1;
-    if (p2Score === winningSCore) {
-      isGameOver = true;
-      p2Display.classList.add('has-text-success');
-      p1Display.classList.add('has-text-danger');
-      p1Button.disabled = true;
-      p2Button.disabled = true;
-    }
-    p2Display.textContent = p2Score;
-  }
+p2.button.addEventListener('click', function () {
+  updateScores(p2, p1);
 });
 
 
@@ -53,13 +53,21 @@ resetButton.addEventListener('click', reset);
 
 function reset() {
   isGameOver = false;
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.textContent = 0;
-  p2Display.textContent = 0;
 
-  p2Display.classList.remove('has-text-success', 'has-text-danger');
-  p1Display.classList.remove('has-text-success', 'has-text-danger');
-  p1Button.disabled = false;
-  p2Button.disabled = false;
+  // p1.score = 0;
+  // p2.score = 0;
+  // p1.display.textContent = 0;
+  // p2.display.textContent = 0;
+
+  // p2.display.classList.remove('has-text-success', 'has-text-danger');
+  // p1.display.classList.remove('has-text-success', 'has-text-danger');
+  // p1.button.disabled = false;
+  // p2.button.disabled = false;
+
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.textContent = 0;
+    p.display.classList.remove('has-text-success', 'has-text-danger');
+    p.button.disabled = false;
+  }
 };
